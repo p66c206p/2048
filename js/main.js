36e129baf89aaa7a8b53c846100bd88b994b5f36
p66c206p <42195846+p66c206p@users.noreply.github.com>
2018-08-30 12:03:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ var touchStartX, touchStartY, touchMoveX, touchMoveY;
 
 // - main -
 window.onload = function() {
-  console.log(2);
   gameStart();
+  console.log(3);
 
   // イベントの登録
   newGame.addEventListener('click', gameStart);
-  window.addEventListener('keydown', keyDown);            // キー操作
-  gameBoard.addEventListener('touchstart', touchStart);   // フリック操作
-  gameBoard.addEventListener('touchmove', touchMove);
-  gameBoard.addEventListener('touchend', touchEnd);
+  window.addEventListener('keydown', moveBoardWithKey);
+  gameBoard.addEventListener('touchstart', flickStart);
+  gameBoard.addEventListener('touchmove', flicking);
+  gameBoard.addEventListener('touchend', moveBoardWithFlick);
 }
 
 // - event -
@@ -27,34 +27,34 @@ function gameStart() {
   board.initialize();
 }
 
-function keyDown(event) {
-  var direction = {
-    '37' : 'left' ,   // key: [←]
-    '38' : 'up'   ,   // key: [↑]
-    '39' : 'right',   // key: [→]
-    '40' : 'down'     // key: [↓]
+function moveBoardWithKey(event) {
+  var keyDirection = {
+    '37' : 'left' ,   // [←]
+    '38' : 'up'   ,   // [↑]
+    '39' : 'right',   // [→]
+    '40' : 'down'     // [↓]
   };
 
-  if (direction[event.keyCode]) {
-    board.move(direction[event.keyCode]);
+  if (keyDirection[event.keyCode]) {
+    board.move(keyDirection[event.keyCode]);
   }
 }
 
-function touchStart(event) {
-  event.preventDefault();
+function flickStart(event) {
+  // event.preventDefault();
 
   touchStartX = event.touches[0].pageX;
   touchStartY = event.touches[0].pageY;
 }
 
-function touchMove(event) {
+function flicking(event) {
   event.preventDefault();
 
   touchMoveX = event.changedTouches[0].pageX;
   touchMoveY = event.changedTouches[0].pageY;
 }
 
-function touchEnd(event) {
+function moveBoardWithFlick(event) {
   var XLength = touchMoveX - touchStartX;
   var YLength = touchMoveY - touchStartY;
   var XMoveThanY = (Math.abs(XLength) > Math.abs(YLength));
