0b19b4eaca2072354bd9ffa05d373c0ce1db8ac3
p66c206p <42195846+p66c206p@users.noreply.github.com>
2024-07-01 15:43:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ function checkGameState() {
   if (BOARD.isJustCleared()) message = "ゲームクリアです！";
   if (BOARD.isGameOver())    message = "ゲームオーバーです。";
 
-  if (message) {
+  if (message != null) {
     setTimeout('alert(message)', 300);
   }
 }
